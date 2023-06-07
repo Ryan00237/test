bd40211f9886fd407580b94d260e083fa9364816
Ryan00237 <75231025+Ryan00237@users.noreply.github.com>
2023-06-07 22:18:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,70 +1,84 @@
+import time
 import psutil
 import threading
-import time
-import datetime
 import subprocess
 from collections import defaultdict
+from statistics import mean
+from datetime import datetime
 
 
-class PerformanceMonitor:
-    def __init__(self, keywords, file_path):
+class Monitor:
+    def __init__(self, keywords, duration, interval=1):
         self.keywords = keywords
-        self.file_path = file_path
-        self.stop_flag = False
-        self.data = defaultdict(list)
+        self.duration = duration
+        self.interval = interval
+        self.data = defaultdict(lambda: defaultdict(list))
+        self.filename = self.generate_filename()
+        self.start_time = None
+        self.end_time = None
+
+    def generate_filename(self):
+        timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        filename = f"{'_'.join(self.keywords)}_{timestamp}.txt"
+        return filename
 
     def start(self):
-        self.thread = threading.Thread(target=self.collect_data)
-        self.thread.start()
+        self.start_time = datetime.now()
+        start_time = time.time()
+        while time.time() - start_time < self.duration:
+            self.collect_data()
+            time.sleep(self.interval)
+        self.end_time = datetime.now()
+        self.save_data()
+        self.summarize_data()
 
     def collect_data(self):
-        while not self.stop_flag:
-            for proc in psutil.process_iter(['name', 'cpu_percent', 'memory_info']):
-                for keyword in self.keywords:
-                    if keyword.lower() in proc.info['name'].lower():
-                        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                        cpu = round(proc.info['cpu_percent'] / psutil.cpu_count(), 2)  # Convert to percentage
-                        memory = round(proc.info['memory_info'].rss / (1024 ** 3), 2)  # Convert to GB
-                        gpu_memory = self.get_gpu_memory()  # Get GPU memory usage
-                        self.data[keyword].append((timestamp, cpu, memory, gpu_memory))
-                        with open(self.file_path, 'a') as f:
-                            f.write(
-                                f"{timestamp}, {keyword}, CPU: {cpu}%, Memory: {memory}GB, GPU Memory: {gpu_memory}GB\n")
-            time.sleep(1)
+        cpu_percent = round(psutil.cpu_percent(), 2)
+        mem_info = round(psutil.virtual_memory().used / (1024 ** 3), 2)  # Convert to GB and round
 
-    def stop(self):
-        self.stop_flag = True
-        self.thread.join()
-        self.calculate_statistics()
+        self.data["system"]["cpu"].append(cpu_percent)
+        self.data["system"]["memory"].append(mem_info)
 
-    def calculate_statistics(self):
-        for keyword, data in self.data.items():
-            cpu_values = [x[1] for x in data]
-            memory_values = [x[2] for x in data]
-            gpu_memory_values = [x[3] for x in data]
-            cpu_max = max(cpu_values)
-            cpu_min = min(cpu_values)
-            cpu_avg = round(sum(cpu_values) / len(cpu_values), 2)
-            memory_max = max(memory_values)
-            memory_min = min(memory_values)
-            memory_avg = round(sum(memory_values) / len(memory_values), 2)
-            gpu_memory_max = max(gpu_memory_values)
-            gpu_memory_min = min(gpu_memory_values)
-            gpu_memory_avg = round(sum(gpu_memory_values) / len(gpu_memory_values), 2)
-            print(f"{keyword}:\nCPU - Max: {cpu_max}%, Min: {cpu_min}%, Avg: {cpu_avg}%")
-            print(f"Memory - Max: {memory_max}GB, Min: {memory_min}GB, Avg: {memory_avg}GB")
-            print(f"GPU Memory - Max: {gpu_memory_max}GB, Min: {gpu_memory_min}GB, Avg: {gpu_memory_avg}GB\n")
+        for keyword in self.keywords:
+            cpu_percent = 0.0
+            mem_info = 0.0
+            for proc in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_info', 'cmdline']):
+                if keyword in (' '.join(proc.info['cmdline']) if proc.info['cmdline'] else proc.info['name']):
+                    cpu_percent += proc.info['cpu_percent']
+                    mem_info += proc.info['memory_info'].rss / (1024 ** 3)  # Convert to GB
 
-    def get_gpu_memory(self):
-        try:
-            output = subprocess.check_output(['nvidia-smi', '--query-gpu=memory.used', '--format=csv,noheader,nounits'])
-            return round(float(output.decode('utf-8').strip()) / 1024, 2)  # Convert to GB
-        except Exception as e:
-            print(f"Error getting GPU memory usage: {e}")
-            return 0.0
+            self.data[keyword]['cpu'].append(round(cpu_percent, 2))
+            self.data[keyword]['memory'].append(round(mem_info, 2))
+
+    def save_data(self):
+        with open(self.filename, "w") as f:
+            f.write(f'Start Time: {self.start_time.strftime("%Y.%m.%d %H:%M:%S")}\n')
+            for key, values in self.data.items():
+                f.write(f'--- {key} ---\n')
+                for metric, readings in values.items():
+                    f.write(f'{metric}: {readings}\n')
+                f.write('\n')
+            f.write(f'End Time: {self.end_time.strftime("%Y.%m.%d %H:%M:%S")}\n')
+
+    def summarize_data(self):
+        summary = {}
+        for key, values in self.data.items():
+            summary[key] = {k: {"min": round(min(v), 2),
+                                "max": round(max(v), 2),
+                                "avg": round(mean(v), 2)}
+                            for k, v in values.items()}
+        with open(self.filename, "a") as f:
+            f.write('--- Summary ---\n')
+            print('--- Summary ---')
+            for key, value in summary.items():
+                f.write(f'{key}: {value}\n')
+                print(f'{key}: {value}')
+
+    # def get_gpu_memory(self):
+    #     result = subprocess.run(["nvidia-smi", "--query-gpu=memory.used", "--format=csv,nounits,noheader"],
+    #                             stdout=subprocess.PIPE)
+    #     return sum(float(x) for x in result.stdout.decode().strip().split('\n')) / 1024  # Convert to GB
 
 
-monitor = PerformanceMonitor(["Chrome", "python", "iTerm", "pycharm", "download"], "performance.txt")
+monitor = Monitor(["pycharm", "Chrome"], 10)
 monitor.start()
-time.sleep(10)  # Collect data for 10 seconds
-monitor.stop()
